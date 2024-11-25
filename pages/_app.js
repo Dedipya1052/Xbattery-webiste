c91821c695376171622441931a735e46358499e3
chiluveru saikiran <chiluverusaikiran@chiluverus-MacBook-Pro.local>
2024-11-25 11:07:04 +0530

    Add google tag in app.js

--- pages/_app.js
+++ pages/_app.js
@@ -21,6 +21,24 @@ export default function App({ Component, pageProps }) {
   return (
     <div className={isHomePage ? "bg-[black] " : "bg-white text-black"}>
       <Head>
+        
+         {/* Google Tag (gtag.js) */}
+         <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-Y419512V0Q"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-Y419512V0Q');
+            `,
+          }}
+          defer
+        ></script>
+
         <link rel="icon" type="image/webp" href="/favicon.webp" />
         {!display && <link rel="canonical" href={currentURL} />}
       </Head>
